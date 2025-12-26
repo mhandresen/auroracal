6f9c99f3d84facbf2fbac9c4e584bf4f8bffc169
Martin Hellebostad Andresen <martin.hellebostad.andresen@intility.no>
2025-12-26 21:08:19 +0100

    chore: add vercel and correct port logic

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .build();
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://auroracal-web.vercel.app'],
     credentials: true,
   });
   app.use(cookieParser());
@@ -20,6 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(Number(process.env.PORT || 3000), '0.0.0.0');
 }
 void bootstrap();
